3f6134ee01563725b726e71d4d35a64ca684170e
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-11-14 20:43:44 -0300

    fix: fixed all

---
+++ src/main/java/fiap/com/br/gs_java/painel/PainelSolar.java
@@ -0,0 +1,15 @@
+package fiap.com.br.gs_java.painel;
+
+
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Cacheable
+@NoArgsConstructor
+//@Table
+public class PainelSolar {
+}
