799df6eec10f442937d1319b00b5523cc18ef27d
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-11-21 22:25:02 -0300

    fix

--- src/main/java/br/com/fiap/gs_java/auth/TokenService.java
+++ src/main/java/br/com/fiap/gs_java/auth/TokenService.java
@@ -4,6 +4,7 @@ import br.com.fiap.gs_java.usuario.Usuario;
 import br.com.fiap.gs_java.usuario.UsuarioRepository;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,15 +13,16 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 @Service
+@RequiredArgsConstructor
 public class TokenService {
 
     public Algorithm ALGORITHM;
     private final UsuarioRepository usuarioRepository;
 
-    public TokenService(UsuarioRepository usuarioRepository, @Value("${jwt.secret}") String secret) {
-      this.usuarioRepository = usuarioRepository;
-     this.ALGORITHM = Algorithm.HMAC256(secret);
-     }
+    //public TokenService(UsuarioRepository usuarioRepository, @Value("${jwt.secret}") String secret) {
+      //this.usuarioRepository = usuarioRepository;
+     //this.ALGORITHM = Algorithm.HMAC256(secret);
+     //}
 
     public Token createToken(Credentials credentials) {
         var expiresAt = LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.ofHours(-3));
